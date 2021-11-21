6ea18bd84891c6b7b734c6358d3151798ea3fdf0
alexpikman <73315445+alexpikman@users.noreply.github.com>
2021-11-21 19:25:08 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,3 +1,30 @@
+import sys
+import os
+import time
+import socket
+import random
+import pyfiglet
+
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+out = pyfiglet.figlet_format('CyberPQ')
+print(out)
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+
+
+
 a = int(input('Number?'))
 if a < -5:
     print('Low')
